50fec601f0c32eb30d02c715e2e7970a592c6241
mafevito <mafevito@hotmail.com>
2017-08-10 17:16:52 +0200

    Miguel was here to push my webpage to the stars... with bloooood

--- server/app.js
+++ server/app.js
@@ -9,18 +9,6 @@ require('./passport/local')(passport);
 require('./config/express')(app);
 require('./config/cors')(app);
 
-// var whitelist = [
-//     'http://localhost:4200',
-// ];
-// var corsOptions = {
-//     origin: function(origin, callback){
-//         var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-//         callback(null, originIsWhitelisted);
-//     },
-//     credentials: true
-// };
-// app.use(cors(corsOptions));
-
 const authRoutes = require('./routes/auth');
 const socilitudRoutes = require('./routes/solicitude');
 const relationSolicitudeUser = require('./routes/relationSolicitudeUser');
